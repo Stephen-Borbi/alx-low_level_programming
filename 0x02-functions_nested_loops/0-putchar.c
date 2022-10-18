9931819b01cdcd538ee92308e2ed490be0587b2f
xorladem <stephenborbi@gmail.com>
2022-10-18 06:27:49 -0700

    prints _puchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "main";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
