242c7bc6b8ea3be070beda3fc433f57055f1fa55
xorladem <113623998+xorladem@users.noreply.github.com>
2022-11-16 16:34:17 +0000

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,24 @@
-#include "main.h"
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-  * main - define function
-  * @void - describe argument
-  * Return: what does it return?
-  */
-int main(void)
+ * main - prints the opcodes
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	return (0);
 }
-
