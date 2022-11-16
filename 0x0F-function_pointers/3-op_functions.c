f6c7de345e662fe4145524bfc4c45a6684c550f3
xorladem <113623998+xorladem@users.noreply.github.com>
2022-11-16 16:24:37 +0000

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,28 +1,72 @@
-#include <stdlib.h>
 #include "3-calc.h"
 /**
-  * get_op_func - decides which math to use based on the operator
-  * @s: pointer to string
-  * Return: result of the operation
-  */
-int (*get_op_func(char *s))(int, int)
+ * op_add - the sum of two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of two integers
+ */
+int op_add(int a, int b)
 {
-	int i;
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	i = 0;
-	while (i < 5)
-	{
-		if (*ops[i].op == *s && !(*(s + 1)))
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
+	return (a + b);
 }
+/**
+ * op_sub - difference of two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the difference of two integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product of two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the product of two integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division of two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the division of two integers
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+	{
+		return (a / b);
+	}
+}
+/**
+ * op_mod - remainder of the division of two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the remainder of the division of two integers
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+	{
+		return (a % b);
+	}
 }
