ccd56f3262297c43122800cd6418d855f2908bd7
xorladem <113623998+xorladem@users.noreply.github.com>
2022-11-16 16:52:03 +0000

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,11 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - prints the opcodes
- * @argc: argument counter
- * @argv: argument vector
+ * op_sub -
  *
- * Return: Always 0 (Sucess)
+ * Return: 
  */
-int main(int argc, char *argv[])
+int op_sub(int a, int b)
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	if (atoi(argv[1]) < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	return (0);
+
 }
